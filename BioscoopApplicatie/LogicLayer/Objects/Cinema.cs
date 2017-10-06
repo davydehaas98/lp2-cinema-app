75817e2c0bc280634ad47a52897cc569c8efab73
Davy de Haas <d.dehaas@student.fontys.nl>
2017-10-06 13:00:26 +0200

    Add id to Cinema

--- BioscoopApplicatie/LogicLayer/Objects/Cinema.cs
+++ BioscoopApplicatie/LogicLayer/Objects/Cinema.cs
@@ -8,13 +8,15 @@ namespace LogicLayer
 {
     public class Cinema
     {
+        private int id;
         private string name;
         private string type;
         private List<Seat> seats;
+        public int Id { get { return this.id; } set { this.id = value; } }
         public string Name { get { return this.name; } set { this.name = value; } }
         public string Type { get { return this.type; } set { this.type = value; } }
         public List<Seat> Seats { get { return this.seats; } set { this.seats = value; } }
-        public Cinema(string name, string type, List<Seat> seats)
+        public Cinema(int id, string name, string type, List<Seat> seats)
         {
             this.name = name;
             this.type = type;
